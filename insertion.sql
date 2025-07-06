7ddad87de7e75ccd297be81bd02ecf5c75e30d8a
manivarshith28 <manivarshith2806@gmail.com>
2025-07-06 21:21:31 +0530

    inserting values into employees table

---
+++ insertion.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (empID, firstName, lastName, age, department, salary)
+VALUES
+(1, 'Amit', 'Sharma', 30, 'Sales', 55000.00),
+(2, 'Priya', 'Patel', 28, 'Marketing', 62000.00),
+(3, 'Ravi', 'Iyer', 35, 'IT', 72000.00),
+(4, 'Neha', 'Gupta', 40, 'HR', 75000.00),
+(5, 'Suresh', 'Kumar', 38, 'Sales', 58000.00);
